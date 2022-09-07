ee18c639ab8dbd9cd1c252ebccdaad37d3a8b3db
EhsonZokirov <zokirov_2000@mail.ru>
2022-09-07 18:12:44 +0500

    First Commit

---
+++ HomeTask/OOP & class/geometry.js
@@ -0,0 +1,13 @@
+class geometry {
+    constructor(x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+    }
+    str() {
+        return `[x=${this.x}, y=${this.y}, w=${this.w}, h=${this.h}]`
+    }
+}
+let geo = new geometry(1, 2, 3, 4)
+console.log(geo.str());
---
+++ HomeTask/OOP & class/videoPlans.js
@@ -0,0 +1,25 @@
+class BasicPlan {
+    static canStream = true;
+    static canDownload = true;
+    static hasSD = true;
+    static hasHD = false;
+    static hasUHD = false;
+    static numOfDevices = 1;
+    static price = '$8.99';
+
+}
+class StandartPlan extends BasicPlan {
+    static hasHD = true
+    static numOfDevices = 2
+    static price = "$12.99"
+}
+
+class PremiumPlan extends StandartPlan {
+    static hasUHD = true
+    static numOfDevices = 4
+    static price = "$15.99"
+
+}
+console.log(BasicPlan.price);   // $8.99
+console.log(StandartPlan.price);// $12.99
+console.log(PremiumPlan.price); // $15.99
