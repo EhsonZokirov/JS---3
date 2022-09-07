754e18b70d3279c2f24bce95a6022febd31d1f80
EhsonZokirov <zokirov_2000@mail.ru>
2022-09-07 19:16:03 +0500

    add new example

--- HomeTask/OOP & class/videoPlans.js
+++ HomeTask/OOP & class/videoPlans.js
@@ -1,3 +1,4 @@
+/*
 class BasicPlan {
     static canStream = true;
     static canDownload = true;
@@ -15,6 +16,45 @@ class StandartPlan extends BasicPlan {
 }
 
 class PremiumPlan extends StandartPlan {
+    static hasUHD = true
+    static numOfDevices = 4
+    static price = "$15.99"
+
+}
+console.log(BasicPlan.price);   // $8.99
+console.log(StandartPlan.price);// $12.99
+console.log(PremiumPlan.price); // $15.99
+*/
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+////////////////// bez extends //////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+class BasicPlan {
+    static canStream = true;
+    static canDownload = true;
+    static hasSD = true;
+    static hasHD = false;
+    static hasUHD = false;
+    static numOfDevices = 1;
+    static price = '$8.99';
+
+}
+class StandartPlan extends BasicPlan {
+    static canStream = true;
+    static canDownload = true;
+    static hasSD = true;
+    static hasHD = true
+    static hasUHD = false
+    static numOfDevices = 2
+    static price = "$12.99"
+}
+
+class PremiumPlan extends StandartPlan {
+    static canStream = true;
+    static canDownload = true;
+    static hasSD = true;
+    static hasHD = true
     static hasUHD = true
     static numOfDevices = 4
     static price = "$15.99"
