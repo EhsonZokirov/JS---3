c3edb7f58787938df1d3b6bc665ed260d7c2086f
EhsonZokirov <zokirov_2000@mail.ru>
2022-09-08 19:02:26 +0500

    first commit

---
+++ ClassWork/OBJECT/Tasks/richMens.js
@@ -0,0 +1,38 @@
+let persons =
+    [
+        { name: 'Romiz', price: 1000, valyute: 'TJS', },
+        { name: 'Messi', price: 150, valyute: 'TJS', },
+        { name: 'Ronaldo', price: 230, valyute: 'TJS', },
+        { name: 'Jony', price: 1234, valyute: 'USD', },
+        { name: 'Max', price: 333, valyute: 'USD', },
+        { name: 'John', price: 780, valyute: 'USD', },
+        { name: 'Doe', price: 280, valyute: 'TJS', },
+    ]
+function USD(persons) {
+    let arr1 = []
+    let res = 0
+    for (let i = 0; i < persons.length; i++) {
+        if (persons[i].valyute == 'USD') {
+            arr1.push(persons[i])
+        }
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        res += (Math.ceil(arr1[i].price * 10.20)) // [ 12587, 3397, 7956 ] = 23940
+    }
+    return `sum of USA = ${res} TJS`
+}
+function TJS(persons) {
+    let res = []
+    for (let i = 0; i < persons.length; i++) {
+        if (persons[i].price > 500) {
+            if (persons[i].valyute == 'USD') {
+                continue
+            }
+            res.push(persons[i].price)
+        }
+    }
+    return `sum of TJK = ${res} TJS`
+}
+
+console.log(USD(persons)); // sum = 23 940 somoni
+console.log(TJS(persons)); // sum = 1000 somoni
