1318d0d0d3b0fe2cd6175b4a0bcc434b8ada3b92
EhsonZokirov <zokirov_2000@mail.ru>
2022-09-14 13:05:48 +0500

    edited syntax

--- HomeTask/DOM/lampa.js
+++ HomeTask/DOM/lampa.js
@@ -5,7 +5,6 @@ on.style.display = 'none'
 btn.style.background = 'yellow'
 
 btn.onclick = () => {
-<<<<<<< HEAD
     if (btn.innerHTML == 'ON') {
         btn.innerHTML = 'OFF'
         btn.style.backgroundColor = 'silver'
@@ -18,17 +17,3 @@ btn.onclick = () => {
     on.innerHTML = on.style.display = "none"
     off.innerHTML = off.style.display = 'inline-block'
 }
-=======
-    if (btn.innerHTML == 'on') {
-        btn.innerHTML = 'off'
-        btn.style.backgroundColor = 'silver'
-        off.innerHTML = style.display = "none"
-        on.innerHTML = on.style.display = 'block'
-        return
-    }
-    btn.innerHTML = 'on'
-    btn.style.backgroundColor = 'yellow'
-    on.innerHTML = on.style.display = "none"
-    off.innerHTML = off.style.display = 'block'
-}
->>>>>>> ab0c204dcb75bdd7300693662609b79f7aa1963f
