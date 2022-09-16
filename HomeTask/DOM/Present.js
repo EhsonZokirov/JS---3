97a7600abb67dffbb9ec2b0b46e76e1d97e16202
EhsonZokirov <zokirov_2000@mail.ru>
2022-09-16 19:15:06 +0500

    FIRST COMMIT

---
+++ HomeTask/DOM/Present.js
@@ -0,0 +1,7 @@
+let container = document.createElement(`div`)
+// container.classList.add(`container`)
+container.setAttribute(`id`, `container`)
+container.style.background = `red`
+container.style.width = `200px`
+container.style.height = `200px`
+document.body.appendChild(container)
